4ddc9fda16c94690b556b84cd82cf7fe4aa64816
yuesth <yues_hafiyan@ajari.app>
2024-08-17 23:43:32 +0700

    fix: update round

--- stellar/frontend/src/app/application/create-round/page.tsx
+++ stellar/frontend/src/app/application/create-round/page.tsx
@@ -1023,7 +1023,7 @@ const CreateRoundPage = () => {
 									<div className="flex items-center space-x-2">
 										<div className="bg-grantpicks-black-400 rounded-full w-6 h-6" />
 										<p className="text-sm font-semibold text-grantpicks-black-950">
-											{prettyTruncate(selected.name)}
+											{prettyTruncate(selected.name, 15, 'address')}
 										</p>
 									</div>
 									<IconClose

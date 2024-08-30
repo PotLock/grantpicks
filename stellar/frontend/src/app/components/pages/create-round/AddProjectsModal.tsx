8fec2d2d6b6b7854179998faa80be99a9da558dd
ajiesaw <ajiesaw1612@gmail.com>
2024-08-30 17:57:20 +0700

    feat: open project detail drawer

--- stellar/frontend/src/app/components/pages/create-round/AddProjectsModal.tsx
+++ stellar/frontend/src/app/components/pages/create-round/AddProjectsModal.tsx
@@ -22,6 +22,9 @@ import IconLoading from '../../svgs/IconLoading'
 import { UseFieldArrayAppend, UseFieldArrayRemove } from 'react-hook-form'
 import { CreateRoundData } from '@/types/form'
 import { prettyTruncate } from '@/utils/helper'
+import ProjectDetailDrawer from '../round-vote/ProjectDetailDrawer'
+import { IProjectDetailOwner } from '@/app/round-vote/[roundId]/page'
+import { Project } from 'round-client'
 
 interface AddProjectsModalProps extends BaseModalProps {
 	selectedProjects: IGetProjectsResponse[]
@@ -43,6 +46,8 @@ const AddProjectsModal = ({
 		IGetProjectsResponse[]
 	>([])
 	const [searchProject, setSearchProject] = useState<string>('')
+	const [showProjectDetailDrawer, setShowProjectDetailDrawer] =
+		useState<IProjectDetailOwner>({ isOpen: false, project: null })
 	const onFetchProjects = async (key: { skip: number; limit: number }) => {
 		const contracts = new Contracts(
 			process.env.NETWORK_ENV as Network,
@@ -117,7 +122,7 @@ const AddProjectsModal = ({
 						<div className="overflow-y-auto max-h-[20vh]">
 							{tempSelectedProjects.map((selected, index) => (
 								<div
-									className="flex items-center justify-between p-2 cursor-pointer hover:bg-grantpicks-black-200 transition"
+									className="flex items-center justify-between p-2 hover:bg-grantpicks-black-200 transition"
 									key={index}
 								>
 									<div className="flex items-center space-x-2">
@@ -128,9 +133,18 @@ const AddProjectsModal = ({
 											width={24}
 											height={24}
 										/>
-										<p className="text-base font-normal">
+										<button
+											onClick={() => {
+												setShowProjectDetailDrawer((prev) => ({
+													...prev,
+													isOpen: true,
+													project: selected as Project,
+												}))
+											}}
+											className="text-base font-normal"
+										>
 											{prettyTruncate(selected.name, 20, 'address')}
-										</p>
+										</button>
 									</div>
 									<IconTrash
 										size={24}
@@ -238,6 +252,16 @@ const AddProjectsModal = ({
 					</Button>
 				</div>
 			</div>
+			<ProjectDetailDrawer
+				isOpen={showProjectDetailDrawer?.isOpen || false}
+				onClose={() =>
+					setShowProjectDetailDrawer((prev) => ({
+						...prev,
+						isOpen: false,
+					}))
+				}
+				projectData={showProjectDetailDrawer.project || undefined}
+			/>
 		</Modal>
 	)
 }

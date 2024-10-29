fdadf01f86d83a41635202656023448e17a9597b
ajiesaw <ajiesaw1612@gmail.com>
2024-10-29 13:39:28 +0700

    feat: change newsletter section color and layout

--- stellar/frontend/src/app/components/pages/landing/StayUpdatedSection.tsx
+++ stellar/frontend/src/app/components/pages/landing/StayUpdatedSection.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect } from 'react'
-import Image from 'next/image'
 
 const StayUpdatedSection = () => {
 	useEffect(() => {
@@ -15,12 +14,13 @@ const StayUpdatedSection = () => {
 
 	return (
 		<div className="pt-20 md:pt-24 xl:pt-32 flex flex-col w-full mx-auto">
-			<div className="grid lg:flex lg:items-center z-10 rounded-[40px] py-9 sm:py-[67px] px-9 sm:px-[61px] lg:px-0 lg:pl-[61px] bg-gradient-to-l from-[#9E77F8] via-[#8D5EF7] to-[#743CF6] shadow-[0px_8px_20px_0px_rgba(0,0,0,0.05),0px_4px_12px_0px_rgba(0,0,0,0.1),0px_2px_4px_0px_rgba(0,0,0,0.1),inset_0px_0.5px_0px_0px_rgba(0,0,0,0.1)]">
-				<div className="lg:max-w-[743px] grid gap-y-8">
-					<p className="text-[40px] md:text-[44px] lg:text-[50px] font-semibold">
+			<div className="z-10 flex justify-center rounded-[40px] py-9 sm:py-[67px] px-9 sm:px-[61px] bg-[#743CF6] shadow-[0px_8px_20px_0px_rgba(0,0,0,0.05),0px_4px_12px_0px_rgba(0,0,0,0.1),0px_2px_4px_0px_rgba(0,0,0,0.1),inset_0px_0.5px_0px_0px_rgba(0,0,0,0.1)]">
+				<div className="bg-[url('/assets/images/container.png')] absolute inset-0 h-full opacity-5"></div>
+				<div className="grid gap-y-8 justify-items-center max-w-[743px] z-10">
+					<p className="text-[40px] md:text-[44px] lg:text-[50px] font-semibold text-center">
 						Stay Updated on Funding Opportunities and Innovations!
 					</p>
-					<p className="text-xl">
+					<p className="text-xl text-center">
 						Receive the latest news, updates, and insights from GrantPicks. Be
 						the first to know about new funding rounds and project highlights!
 					</p>
@@ -34,14 +34,6 @@ const StayUpdatedSection = () => {
 							backgroundColor: 'transparent',
 						}}
 					></iframe>
-				</div>
-				<div className="lg:shrink-0 flex justify-center">
-					<Image
-						src="/assets/images/grantpicks-logo-large.png"
-						alt="grantpicks-logo"
-						width={514.68}
-						height={320.51}
-					/>
 				</div>
 			</div>
 		</div>

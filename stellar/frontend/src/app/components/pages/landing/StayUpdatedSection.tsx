b0d81140c683190c22e6b89c4278a1f158e5612b
ajiesaw <ajiesaw1612@gmail.com>
2024-10-29 14:05:16 +0700

    fix: image overlapping with other section

--- stellar/frontend/src/app/components/pages/landing/StayUpdatedSection.tsx
+++ stellar/frontend/src/app/components/pages/landing/StayUpdatedSection.tsx
@@ -14,7 +14,7 @@ const StayUpdatedSection = () => {
 
 	return (
 		<div className="pt-20 md:pt-24 xl:pt-32 flex flex-col w-full mx-auto">
-			<div className="z-10 flex justify-center rounded-[40px] py-9 sm:py-[67px] px-9 sm:px-[61px] bg-[#743CF6] shadow-[0px_8px_20px_0px_rgba(0,0,0,0.05),0px_4px_12px_0px_rgba(0,0,0,0.1),0px_2px_4px_0px_rgba(0,0,0,0.1),inset_0px_0.5px_0px_0px_rgba(0,0,0,0.1)]">
+			<div className="relative z-10 flex justify-center rounded-[40px] py-9 sm:py-[67px] px-9 sm:px-[61px] bg-[#743CF6] shadow-[0px_8px_20px_0px_rgba(0,0,0,0.05),0px_4px_12px_0px_rgba(0,0,0,0.1),0px_2px_4px_0px_rgba(0,0,0,0.1),inset_0px_0.5px_0px_0px_rgba(0,0,0,0.1)]">
 				<div className="bg-[url('/assets/images/container.png')] absolute inset-0 h-full opacity-5"></div>
 				<div className="grid gap-y-8 justify-items-center max-w-[743px] z-10">
 					<p className="text-[40px] md:text-[44px] lg:text-[50px] font-semibold text-center">

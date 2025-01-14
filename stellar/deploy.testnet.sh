28f9d54aa5da76a16f84773a75a8283f4997a343
Boluwatife Popoola <olaoyejnr@gmail.com>
2025-01-15 00:17:29 +0100

    chore: test deploy

--- stellar/deploy.testnet.sh
+++ stellar/deploy.testnet.sh
@@ -11,9 +11,10 @@ echo "registry contract id: $registry_contract_id"
 echo "round & factory contract id: $round_contract_id"
 echo "deployer address: $my_address"
 
-stellar contract invoke --source potlock-testnet --network testnet --id ${lists_contract_id} -- initialize --owner ${my_address} 
+stellar contract invoke --source potlock-testnet --network testnet --id ${lists_contract_id} -- initialize --owner ${my_address}
+stellar contract invoke --source potlock-testnet --network testnet --id ${lists_contract_id} -- create_list --owner ${my_address} --name "Test List" --default_registration_status Approved
 stellar contract invoke --source potlock-testnet --network testnet --id ${registry_contract_id} -- initialize --contract_owner ${my_address} 
-stellar contract invoke --source potlock-testnet --network testnet --id ${round_contract_id} -- initialize --registry_address ${registry_contract_id} --caller ${my_address} --token_address ${native_token} --protocol_fee_basis_points 200 --protocol_fee_recipient ${my_address} --default_page_size 5 --list_address ${lists_contract_id} --kyc_list_id 1
+stellar contract invoke --source potlock-testnet --network testnet --id ${round_contract_id} -- initialize --registry_address ${registry_contract_id} --caller ${my_address} --token_address ${native_token} --protocol_fee_basis_points 200 --protocol_fee_recipient ${my_address} --default_page_size 5 --list_address ${lists_contract_id} --voting_wl_list_id 1
 
 # stellar contract bindings typescript --network testnet --contract-id $lists_contract_id --output-dir ./packages/lists-client --overwrite
 # stellar contract bindings typescript --network testnet --contract-id $registry_contract_id --output-dir ./packages/project-registry-client --overwrite
